1b82996ea389beed4a8bdf1eb3804ee36a3d47c4
mohit1707 <mehtam2701@gmail.com>
2023-08-20 22:52:34 +0530

    added redux state management to movie details page, commented  the loader method in react router

--- src/routes/movie.jsx
+++ src/routes/movie.jsx
@@ -2,9 +2,14 @@
 import { useLoaderData } from "react-router-dom";
 import { getMovieCast, getMovieDetails } from "../lib/movies.utils";
 import AppImage from "../components/AppImage";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Skeleton, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { setMovieDetails } from "../store/movies/moviesSlice";
+import MovieDetailSkeleton from "../components/Skeletons/MovieDetailSkeleton";
 
 export async function loader({ params }) {
 	const movie = await getMovieDetails(params.movieId);
@@ -17,104 +22,64 @@ export async function loader({ params }) {
 	return { movie };
 }
 
-/**
- * 
- * {
-    "adult": false,
-    "backdrop_path": "/rRcNmiH55Tz0ugUsDUGmj8Bsa4V.jpg",
-    "belongs_to_collection": null,
-    "budget": 45000000,
-    "genres": [
-        {
-            "id": 35,
-            "name": "Comedy"
-        },
-        {
-            "id": 10749,
-            "name": "Romance"
-        }
-    ],
-    "homepage": "https://www.nohardfeelingsmovie.com",
-    "id": 884605,
-    "imdb_id": "tt15671028",
-    "original_language": "en",
-    "original_title": "No Hard Feelings",
-    "overview": "On the brink of losing her childhood home, Maddie discovers an intriguing job listing: wealthy helicopter parents looking for someone to “date” their introverted 19-year-old son, Percy, before he leaves for college. To her surprise, Maddie soon discovers the awkward Percy is no sure thing.",
-    "popularity": 1812.181,
-    "poster_path": "/4K7gQjD19CDEPd7A9KZwr2D9Nco.jpg",
-    "production_companies": [
-        {
-            "id": 117188,
-            "logo_path": null,
-            "name": "Excellent Cadaver",
-            "origin_country": "US"
-        },
-        {
-            "id": 86647,
-            "logo_path": null,
-            "name": "Odenkirk Provissiero Entertainment",
-            "origin_country": "US"
-        },
-        {
-            "id": 5,
-            "logo_path": "/2I2kWmd0GCZaxaRu8DVcP2xD861.png",
-            "name": "Columbia Pictures",
-            "origin_country": "US"
-        }
-    ],
-    "production_countries": [
-        {
-            "iso_3166_1": "US",
-            "name": "United States of America"
-        }
-    ],
-    "release_date": "2023-06-15",
-    "revenue": 82995526,
-    "runtime": 104,
-    "spoken_languages": [
-        {
-            "english_name": "English",
-            "iso_639_1": "en",
-            "name": "English"
-        }
-    ],
-    "status": "Released",
-    "tagline": "Pretty. Awkward.",
-    "title": "No Hard Feelings",
-    "video": false,
-    "vote_average": 7.1,
-    "vote_count": 669
-}
- */
+const mapState = ({ movies }) => ({
+	movie: movies.movieDetails,
+});
+
 export default function MoviePage() {
-	const { movie } = useLoaderData();
+	const { movie } = useSelector(mapState);
+
+	const dispatch = useDispatch();
+	const params = useParams();
+	const movieId = params.movieId;
 	console.log({
 		movie,
+		params,
 	});
 	const [cast, setCast] = useState([]);
 	const [director, setDirector] = useState([]);
 	const [showMore, setShowMore] = useState(true);
+	const [isPageLoading, setIsPageLoading] = useState(true);
+	const [isActorsLoading, setIsActorsLoading] = useState(true);
 	const handleClickShowMore = () => {
 		setShowMore(!showMore);
 		console.log({ showMore });
 	};
 
 	const actors = Array.isArray(cast) && showMore ? cast.slice(0, 10) : cast;
+	const handleFetchMovieDetails = () => {
+		setIsPageLoading(true);
+		getMovieDetails(params.movieId)
+			.then((json) => {
+				console.log({ movie: json });
+				setIsPageLoading(false);
+				dispatch(setMovieDetails(json));
+				handleFetchMovieCast(json);
+			})
+			.catch((error) => console.error(error));
+	};
+	const handleFetchMovieCast = (movie) => {
+		setIsActorsLoading(true);
+		getMovieCast(movie.id)
+			.then((json) => {
+				setIsActorsLoading(false);
+				console.log({ cast: json });
+				setCast(json.cast);
+				const director = json.crew.find(
+					(item) => item.job === "Director",
+				);
+				setDirector(director);
+				console.log({ director });
+			})
+			.catch((error) => console.log({ error }));
+	};
 	useEffect(() => {
-		if (movie) {
-			getMovieCast(movie.id)
-				.then((json) => {
-					console.log({ cast: json });
-					setCast(json.cast);
-					const director = json.crew.find(
-						(item) => item.job === "Director",
-					);
-					setDirector(director);
-					console.log({ director });
-				})
-				.catch((error) => console.log({ error }));
-		}
-	}, [movie]);
+		handleFetchMovieDetails();
+	}, [movieId]);
+
+	if (isPageLoading) {
+		return <MovieDetailSkeleton />;
+	}
 	return (
 		<Grid container spacing={3}>
 			<Grid
@@ -161,45 +126,51 @@ export default function MoviePage() {
 					{movie.runtime % 60} min | {director.name}{" "}
 					{/* director to be added */}
 				</Typography>
-				<Typography
-					variant="body1"
-					sx={{
-						mt: 1,
-						"& span": {
-							mr: 1,
-						},
-					}}
-				>
-					<b>Cast:</b>
-					<span>
-						{" "}
-						{Array.isArray(actors) &&
-							actors
-								// .slice(0, 10)
-								.map((item) => item.name)
-								.join(", ")}
-					</span>
-					{Array.isArray(cast) && cast.length > 10 && (
-						<Button
-							sx={{
-								color: (theme) => theme.palette.blue,
-								textTransform: "none",
-								p: 0,
-								px: 0,
-								// ml: 1,
-								
-								mt: "-2px",
-								"&:hover": {
-									background: "transparent",
-								},
-							}}
-							onClick={handleClickShowMore}
-						>
-							Show {showMore ? "More" : "Less"}
-						</Button>
-					)}
-					{/* to be added */}
-				</Typography>
+				{isActorsLoading ? (
+					<>
+						<Skeleton height={40} />
+					</>
+				) : (
+					<Typography
+						variant="body1"
+						sx={{
+							mt: 1,
+							"& span": {
+								mr: 1,
+							},
+						}}
+					>
+						<b>Cast:</b>
+						<span>
+							{" "}
+							{Array.isArray(actors) &&
+								actors
+									// .slice(0, 10)
+									.map((item) => item.name)
+									.join(", ")}
+						</span>
+						{Array.isArray(cast) && cast.length > 10 && (
+							<Button
+								sx={{
+									color: (theme) => theme.palette.blue,
+									textTransform: "none",
+									p: 0,
+									px: 0,
+									// ml: 1,
+
+									mt: "-2px",
+									"&:hover": {
+										background: "transparent",
+									},
+								}}
+								onClick={handleClickShowMore}
+							>
+								Show {showMore ? "More" : "Less"}
+							</Button>
+						)}
+						{/* to be added */}
+					</Typography>
+				)}{" "}
 				<Typography variant="body1" sx={{ mt: 2 }}>
 					Description: {movie.overview}
 				</Typography>
